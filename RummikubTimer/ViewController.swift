722e8966999384512a4aaf654e458d3629e426eb
Bart <bartosz.gejgal@gmail.com>
2020-09-02 16:05:39 +0200

    Finished the first version of the app

--- RummikubTimer/ViewController.swift
+++ RummikubTimer/ViewController.swift
@@ -7,14 +7,69 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var startTimerButton: UIButton!
+    @IBOutlet weak var pauseTimerButton: UIButton!
+    
+    var timer = Timer()
+    var counter = 60.0
+    var isPaused = false
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func timerButtonPressed(_ sender: UIButton) {
+        
+        counter = 60.0 // resetting the counter
+        timer.invalidate() // invalidating current timer if there is one
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+        updateTimerButtonText(with: "\(counter)")
+    }
+    
+    @IBAction func pauseTimerButtonPressed(_ sender: UIButton) {
+        if isPaused { // resuming
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+            isPaused = false
+            pauseTimerButton.setTitle("Pause", for: .normal)
+            startTimerButton.isEnabled = true
+        } else { // pausing
+            timer.invalidate()
+            isPaused = true
+            pauseTimerButton.setTitle("Resume", for: .normal)
+            startTimerButton.isEnabled = false
+        }
+    }
+    
+    @objc func updateCounter(){
+        if counter <= 0.0 {
+            timer.invalidate()
+            updateTimerButtonText(with: "Time's up!")
+            playSound()
+        } else {
+            counter = round((counter - 0.1) * 10) / 10
+            updateTimerButtonText(with: "\(counter)")
+        }
+    }
+    
+    func updateTimerButtonText(with text: String){
+        UIView.performWithoutAnimation {
+            startTimerButton.setTitle("\(text)", for: .normal)
+            startTimerButton.layoutIfNeeded()
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "TimesUpSound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
